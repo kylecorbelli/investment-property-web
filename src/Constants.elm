72c5272dde0de50459fdbdb1e3e83f4ba39a7c2c
Kyle Corbelli <kyle.corbelli@gmail.com>
2018-02-17 15:13:22 -0800

    Integrate Zillow API

--- src/Constants.elm
+++ src/Constants.elm
@@ -4,3 +4,7 @@ module Constants exposing (..)
 errorMessageCountdownSeconds : Int
 errorMessageCountdownSeconds =
     5
+
+genericErrorMessage : String
+genericErrorMessage =
+    "Hhmmm... looks like something went wrong."
--- src/Msgs.elm
+++ src/Msgs.elm
@@ -1,8 +1,17 @@
 module Msgs exposing (..)
 
+import Models exposing (..)
+import RemoteData exposing (WebData)
 
 type Msg
     = UpdatePurchasePriceFormField String
     | UpdateGrossMonthlyRentFormField String
     | CommitInputs
     | AdvanceErrorMessageCountdown
+    | UpdateZillowSearchAddressField String
+    | ZillowSearchResultsRequestSent
+    | ZillowSearchResultsRequestCompleted (WebData ZillowSearchResult) -- again, probably use RemoteData
+    | UseZillowDataInAnalysis
+    | ToggleModal
+    | DismissZillowModal
+    | DismissZillowSearchResult
